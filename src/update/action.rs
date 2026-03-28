273a4ed9d9ec5b0edd815399cf6c5dd610aed08f
EthanDGee <workethangee@gmail.com>
2026-03-27 19:37:12 -0600

    feat: added Actions enum
    It will handle all message passing. It currently has actions for the
    following categories:
    - Navigation
    - Board Picker
    - Board View
    - Task Actions
    - Modal actions

---
+++ src/update/action.rs
@@ -0,0 +1,60 @@
+/// Handle message actions as part of Elm Architecture
+pub enum Action {
+    // Navigation
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight,
+    //  MoveToStart,  Post-MVP
+    //  MoveToEnd,  Post-MVP
+
+    // Board Picker Actions
+    OpenBoard,
+    CreateBoard,
+    RenameBoard(String),
+    DeleteBoard,
+    MoveBoardUp,
+    MoveBoardDown,
+
+    // Board View Actions
+    NextColumn,
+    PrevColumn,
+    NextTask,
+    PrevTask,
+
+    // Column Actions
+    CreateColumn,
+    RenameColumn(String),
+    DeleteColumn,
+    MoveColumnLeft,
+    MoveColumnRight,
+
+    // Task Actions
+    CreateTask,
+    EditTask(InputField, String),
+    DeleteTask,
+    ToggleCompletion,
+    MoveTaskUp,
+    MoveTaskDown,
+    MoveTaskToNextColumn,
+    MoveTaskToPrevColumn,
+
+    // Modal Actions
+    // OpenModal(ModalType),
+    CloseModal,
+    ConfirmAction,
+    CancelAction,
+
+    // System
+    Render,
+    Quit,
+}
+
+/// All possible text input fields within the app
+enum InputField {
+    BoardName,
+    ColumnName,
+    TaskTitle,
+    TaskDescription,
+    TaskItem,
+}
---
+++ src/update/update.rs
@@ -0,0 +1,1 @@
+use crate::update::action::action;
