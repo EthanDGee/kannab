a86901e4186f2fe285fabf6d2fe1d90f4e5e2f2c
EthanDGee <workethangee@gmail.com>
2026-05-07 14:49:52 -0600

    feat: added floating window with minimum size
    This helps solve the issue where windows with text inputs would be too
    small to see the text inputs.

--- src/widgets/floating_window.rs
+++ src/widgets/floating_window.rs
@@ -1,22 +1,36 @@
-use ratatui::layout::{Constraint, Direction, Layout, Rect};
+use ratatui::layout::Rect;
+
+/// Creates the same centered rectangle but in addition to it's percentage it also intakes a flat
+/// unit of the minimum size that the window.
+pub fn centered_rect_minimum_size(
+    percent_x: u16,
+    percent_y: u16,
+    min_width: u16,
+    min_height: u16,
+    rectangle: Rect,
+) -> Rect {
+    let mut width = rectangle.width * percent_x / 100;
+    let mut height = rectangle.height * percent_y / 100;
+
+    if min_width > 0 {
+        width = width.max(min_width).min(rectangle.width);
+    }
+    if min_height > 0 {
+        height = height.max(min_height).min(rectangle.height);
+    }
+
+    let x = rectangle.x + (rectangle.width.saturating_sub(width)) / 2;
+    let y = rectangle.y + (rectangle.height.saturating_sub(height)) / 2;
+
+    Rect {
+        x,
+        y,
+        width,
+        height,
+    }
+}
 
 /// Helper function to create a centered rectangle using up to a certain percentage of the available area.
-pub fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
-    let popup_layout = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([
-            Constraint::Percentage((100 - percent_y) / 2),
-            Constraint::Percentage(percent_y),
-            Constraint::Percentage((100 - percent_y) / 2),
-        ])
-        .split(r);
-
-    Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([
-            Constraint::Percentage((100 - percent_x) / 2),
-            Constraint::Percentage(percent_x),
-            Constraint::Percentage((100 - percent_x) / 2),
-        ])
-        .split(popup_layout[1])[1]
+pub fn centered_rect(percent_x: u16, percent_y: u16, rectangle: Rect) -> Rect {
+    centered_rect_minimum_size(percent_x, percent_y, 0, 0, rectangle)
 }
